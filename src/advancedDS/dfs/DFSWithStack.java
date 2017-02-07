cb507cb6605f2bdaf0c77e0f35da123061cd9b2a
Nitin Chaurasia <gs.nitin@gmail.com>
2017-02-07 00:29:52 -0500

    DFS

---
+++ src/advancedDS/dfs/DFSWithStack.java
@@ -0,0 +1,41 @@
+package advancedDS.dfs;
+
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * Created by Nitin Chaurasia on 2/6/17 at 11:26 PM.
+ */
+public class DFSWithStack {
+
+    private Stack<Vertex> stack;
+
+    public void dfs(List<Vertex> vertexList){
+
+        for(Vertex v : vertexList){
+            if(!v.isVisited()){
+                v.setVisited(true);
+
+                dfsWithStack(v);
+            }
+        }
+    }
+
+    private void dfsWithStack(Vertex rootVertex) {
+        this.stack.push(rootVertex);
+
+        rootVertex.setVisited(true);
+
+        while (!stack.isEmpty()){
+            Vertex currentVertex = this.stack.pop();
+            System.out.print(currentVertex + " ");
+
+            for(Vertex v :  currentVertex.getNeighbour()){
+                if(!v.isVisited()){
+                    v.setVisited(true);
+                    this.stack.push(v);
+                }
+            }
+        }
+    }
+}
---
+++ src/advancedDS/dfs/Vertex.java
@@ -0,0 +1,50 @@
+package advancedDS.dfs;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Nitin Chaurasia on 2/6/17 at 11:25 PM.
+ */
+public class Vertex {
+    private String name;
+    private boolean visited;
+    private List<Vertex> neighbour;
+
+    public Vertex(String name) {
+        this.name = name;
+        this.neighbour = new ArrayList<>();
+    }
+
+    public void addNeighbour(Vertex v){
+        this.neighbour.add(v);
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isVisited() {
+        return visited;
+    }
+
+    public void setVisited(boolean visited) {
+        this.visited = visited;
+    }
+
+    public List<Vertex> getNeighbour() {
+        return neighbour;
+    }
+
+    public void setNeighbour(List<Vertex> neighbour) {
+        this.neighbour = neighbour;
+    }
+
+    @Override
+    public String toString(){
+        return this.name;
+    }
+}
