cd9119e40dc983ee58b86e19077ef7d7e475002f
Nitin Chaurasia <gs.nitin@gmail.com>
2019-04-09 15:11:17 -0400

    Update ArrayList.java
    Revision of Syntax

--- src/a1DataStructures/ds1Arrays/ArrayList.java
+++ src/a1DataStructures/ds1Arrays/ArrayList.java
@@ -4,4 +4,15 @@ package a1DataStructures.ds1Arrays;
  * Created by nitin.chaurasia on 2/20/2017.
  */
 public class ArrayList {
+    public static void main(String args[]){
+        int nums[];
+
+        for (int i = 0; i< 10; i++){
+            nums[i] = i*i;
+        }
+
+        for (int a : nums){
+            System.out.println(a);
+        }
+    }
 }
