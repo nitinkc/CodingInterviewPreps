e933fe72e881ed2a82e9f2f61511bbde056f70ff
Nitin Chaurasia <gs.nitin@gmail.com>
2023-11-29 19:08:55 -0700

    bitwise learning

--- src/main/java/bitwise/NumOfDigits.java
+++ src/main/java/bitwise/NumOfDigits.java
@@ -2,10 +2,24 @@ package bitwise;
 
 public class NumOfDigits {
     public static void main(String[] args) {
-        int i = 123456789;
+        int num = 123456789;
+        int n = (int) Math.floor(Math.log10(num)) + 1;
 
-        int v = (int) Math.floor(Math.log10(i)) + 1;
+        System.out.println("number of digits = " + n);
 
-        System.out.println("v = " + v);
+        int msdMask = (int) Math.pow(10,n-1);
+        for (int i = 0; i < n/2; i++) {//Works well with Even number of digits
+            int lsd = num%10;//Least Significant Digit
+            int msd = num/msdMask;
+            System.out.println(msd + " :: " + lsd);
+
+            num = num % msdMask;//Removing the MSD, remainder is the rest of the number
+            num = num / 10;//Removing the LSD
+
+            //Adjust the msdMask, as 2 digits are already removed
+            msdMask = msdMask/100;
+        }
+
+
     }
 }
